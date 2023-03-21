37925b39ce24e144e49685e02e5dc9b605c0f80e
Vinicius Uliano <48574234+viniciusuliano@users.noreply.github.com>
2023-03-21 18:47:32 -0300

    Update atividadePOO8.py

--- atividadePOO8.py
+++ atividadePOO8.py
@@ -13,10 +13,19 @@ class Retangulo:
         self.ponto = ponto
 
     def centro(self):
-        x = self.ponto + {self.altura} / 2
-        y = self.ponto + {self.largura} / 2
-        return Ponto(x, y)
+        x_centro = self.ponto.x + self.altura  / 2
+        y_centro = self.ponto.y + self.largura / 2
+        return Ponto(x_centro, y_centro)
     
-    
+    def imprimir(self):
+        return f'LARGURA: {self.largura} ALTURA: {self.altura}'
 
+altura = float(input('1- Defina o valor da Altura:  '))
+largura = float(input('2- Defina o valor da Largura: '))
 
+ponto1 = Ponto(10, 20)
+retangulo1 = Retangulo(altura, largura, ponto1)
+
+print(ponto1.imprimiValor())
+print(retangulo1.imprimir())
+print(retangulo1.centro().imprimiValor())
